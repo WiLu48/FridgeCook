3aabc42d56ea53ec1395643fa6207ddf784feeef
Patryk <patryk.dawid.wilk@gmail.com>
2019-02-15 21:03:54 +0000

    Recipes component done to call the API and fetch all of the data.
    This needs styling

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Navbar from './Navbar';
+import Recipes from './Components/Recipes'
 
-ReactDOM.render(<Navbar />, document.getElementById('root'));
+ReactDOM.render(<Recipes />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
