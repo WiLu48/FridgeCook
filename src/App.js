2d2a5f85e84953309f780049e1ea657801f54a14
Patryk <patryk.dawid.wilk@gmail.com>
2019-02-20 17:10:43 +0000

    Routes and pages added.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import { Router, Route } from 'react-router'
 import Navbar from './Components/Layout/Navbar';
 import RecipeList from './Components/Recipes/RecipeList';
+import Homepage from './Pages/Homepage';
+import Recipes from './Pages/Recipes';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Navbar />
-        <RecipeList />
-      </React.Fragment>
+      <Router>
+        <Route path='/' component={Homepage} />
+        <Route path='/recipes' component={Recipes} />
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Homepage from './Pages/Homepage';
+import Recipes from './Pages/Recipes';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/recipes' component={Recipes} />
+        </div>
+    </Router> 
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
