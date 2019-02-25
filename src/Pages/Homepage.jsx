1848ebfb6bb040677772c7d92468446d636546db
Patryk <patryk.dawid.wilk@gmail.com>
2019-02-25 10:40:51 +0000

    testing external API (problem with CORS)

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,13 +1,65 @@
 import React, { Component } from 'react'
-import { TextField, MenuItem } from '@material-ui/core'
+import { TextField, MenuItem, Button } from '@material-ui/core'
+import Axios from 'axios';
 
 
 class Homepage extends Component {
+  state = {
+    response: 'before',
+    error: 'error',
+  }
+
+  testAPI(){
+    var postData = {
+      username: "test@test.com",
+      password: "password"
+    };
+    
+    let axiosConfig = {
+      headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+      }
+    };
+    
+    Axios.post('https://p4tr7k.me/API/Create_User.php', postData, axiosConfig)
+    .then((res) => {
+      console.log("RESPONSE RECEIVED: ", res);
+    })
+    .catch((err) => {
+      console.log("AXIOS ERROR: ", err);
+    })
+  }
+
+  fetchRecipes() {
+    Axios.post("https://p4tr7k.me/API/Create_User.php",
+    {"username":"User1","password":"Pass1"})
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  //   Axios({
+  //     method: 'post',
+  //     url: 'https://p4tr7k.me/API/Create_User.php',
+  //     data: {
+  //       username: 'Fred',
+  //       password: 'Flintstone'
+  //     }
+  //   });
+  // }
 
   render() {
     return (
-      <div>
+      <div style={{textAlign: 'center'}}>
         <h1>Homepage</h1>
+        {this.state.response}
+        <br />
+        {this.state.error}
+        <br />
+        <Button onClick={this.testAPI}>TEST</Button>
       </div>
     )
   }
