116c460e2981141458de4d84480de220492011ba
Patryk <patryk.dawid.wilk@gmail.com>
2019-03-23 04:32:17 +0000

    Working on Dashboard Page

---
+++ src/Components/Recipes/Dashboard/MyRecipesItem.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Grid, Card, CardHeader, CardMedia, CardContent, Typography, CardActionArea, Hidden} from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  card: {
+    width: 310,
+    margin: 10,
+  },
+  cardheader: {
+    textAlign: 'center',
+    width: '75%',
+    margin: 'auto',
+    marginTop: '-25px',
+    background: 'white',
+  },
+  carddesc: {
+    paddingTop: 0,
+    height: '100px',
+  },
+  button: {
+    width: '70%',
+    margin: '0 auto',
+    display: 'block',
+    textAlign: 'center',
+  },
+  container: {
+  },
+});
+
+class MyRecipesItem extends Component {
+    state = {
+      categoryname: {
+        1: 'Dinner',
+        2: 'Breakfast',
+        3: 'Desert'
+      },
+    }
+
+  render() {
+
+    const { classes } = this.props;
+
+    const link = "/recipes/" + this.props.id;
+
+    const img = "http://www.p4tr7k.me/API/Recipes/Rec_Imgs/" + this.props.image;
+
+    
+    return(
+      <div className={classes.container}>
+      <Grid item lg={6}>
+      <Card 
+          className={classes.card}
+          key={this.props.key}
+          >
+          <CardActionArea>
+            <CardMedia
+              style={{height: 0, paddingTop: '56.25%'}}
+              image={img}
+              />
+            <CardContent>
+              <Typography style={{textAlign: 'center', overflow: 'hidden'}} variant="h6">
+                {this.props.name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>         
+          <Button variant="contained" color="primary" className={classes.button} component={props => <Link to={link} {...props}/>} >
+            Full Recipe
+          </Button>
+        </Card>
+      </Grid>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(MyRecipesItem);
--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -1,20 +1,101 @@
 import React, { Component } from 'react'
 import { AuthContext } from '../Components/Auth/Auth';
-import {Button} from '@material-ui/core'
+import {withStyles, Button, Grid, Avatar, Paper, Typography, List, ListItem, ListItemAvatar, ListItemText} from '@material-ui/core'
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import MyRecipesItem from '../Components/Recipes/Dashboard/MyRecipesItem';
 
+const styles = theme => ({
+  main: {
+      width: 'auto',
+      display: 'block', // Fix IE 11 issue.
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up(800 + theme.spacing.unit * 3 * 2)]: {
+          width: 1000,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+      },
+  },
+  paper: {
+      marginTop: theme.spacing.unit * 4,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  btns: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
 
-export default class Dashboard extends Component {
+  }
+})
+
+class DashboardPage extends Component {
   static contextType = AuthContext;
+  state = {
+    recipes: [],
+  }
+
+  componentDidMount(){
+    Axios.get("https://p4tr7k.me/API/Recipes/Recipes.php?uid="+sessionStorage.getItem('ID'))
+    .then(res => {
+      this.setState({
+        recipes: res.data.data,
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+
   render() {
     const {checkAuth} = this.context;
+    const {recipes} = this.state;
+    const {classes} = this.props;
     checkAuth();
     return (
-      <div>
-        <Link to="/dashboard/addrecipe">
-        <Button variant='contained' color='default'>Add new Recipe</Button>
-        </Link>
-      </div>
+      <main className={classes.main}>
+        <Paper className={classes.paper}>
+          <Typography variant="h3">
+          Welcome {sessionStorage.getItem('fname')} {sessionStorage.getItem('lname')}
+          </Typography>
+        </Paper>
+        <Grid container justify="space-between" className={classes.btns}>
+          <Grid item>
+            <Link to="/dashboard/addrecipe">
+            <Button variant='contained' color='default'>Add new Recipe</Button>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Button variant='contained' color='default'>Add new Recipe</Button>
+          </Grid>
+          <Grid item>
+            <Button variant='contained' color='default'>Add new Recipe</Button>
+          </Grid>
+          <Grid item>
+            <Button variant='contained' color='default'>Add new Recipe</Button>
+          </Grid>
+        </Grid>
+        <Grid container
+        justify="center">
+          {recipes.map(recipe => 
+            <MyRecipesItem
+            key={recipe.Recipe_ID}
+            id={recipe.Recipe_ID}
+            name={recipe.Recipe_Name}
+            image={recipe.Recipe_Image}
+            desc={recipe.Recipe_Description}
+            cat={recipe.Category_ID}
+            />
+          )}
+        </Grid>
+       
+        {/* <Avatar src="http://www.p4tr7k.me/API/Recipes/Rec_Imgs/22.jpg" /> */}
+      </main>
     )
   }
 }
+
+export default withStyles(styles)(DashboardPage)
